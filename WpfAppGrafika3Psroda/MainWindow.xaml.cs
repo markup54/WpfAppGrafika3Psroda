fb9305854c7213a62ae712c8f961c3c9f57d9a9f
markup54 <marzenakupka@tlen.pl>
2024-01-03 15:24:50 +0100

    przyciski dalej nazot

--- WpfAppGrafika3Psroda/MainWindow.xaml.cs
+++ WpfAppGrafika3Psroda/MainWindow.xaml.cs
@@ -20,9 +20,45 @@ namespace WpfAppGrafika3Psroda
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<BitmapImage> Images { get; set; }
+        public int Aktualny { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+            przygotujObrazy();
+            Aktualny = 0;
+        }
+
+        private void przygotujObrazy()
+        {
+            Images= new List<BitmapImage>();
+            Images.Add(new BitmapImage(new Uri("grafika/rys1.jpg", UriKind.Relative)));
+            Images.Add(new BitmapImage(new Uri("grafika/rys2.jpg", UriKind.Relative)));
+            Images.Add(new BitmapImage(new Uri("grafika/rys3.jpg", UriKind.Relative)));
+            Images.Add(new BitmapImage(new Uri("grafika/rys4.jpg", UriKind.Relative)));
+            Images.Add(new BitmapImage(new Uri("grafika/rys5.jpg", UriKind.Relative)));
+            Images.Add(new BitmapImage(new Uri("grafika/rys6.jpg", UriKind.Relative)));
+            Images.Add(new BitmapImage(new Uri("grafika/rys7.jpg", UriKind.Relative)));
+            Images.Add(new BitmapImage(new Uri("grafika/rys8.jpg", UriKind.Relative)));
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Aktualny--;
+            if (Aktualny < 0)
+            {
+                Aktualny = Images.Count - 1;
+            }
+            obraz.Source = Images[Aktualny];
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Aktualny++;
+            if (Aktualny == Images.Count)
+                Aktualny = 0;
+            obraz.Source = Images[Aktualny];
         }
     }
 }
